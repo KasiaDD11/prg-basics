4dbaad21eb56b16a4418537e179e98483ed2f2c2
Katarzyna Dubis <dubis.katarzyna0@gmail.com>
2025-11-07 16:29:16 +0100

    zadanka

---
+++ 03-ControlStructures/2_6.py
@@ -0,0 +1,8 @@
+numer=int(input('wpisz liczbe: '))
+
+if numer >0:
+    print(f'numer {numer} jest wiekszy od zera')
+elif numer < 0:
+    print(f'numer{numer} jest mniejszy od zera')
+else:
+    print ('numer to zero')
